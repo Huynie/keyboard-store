4efda198855ec7d3384b6838580654f78f3d3650
Huynie <huychaudesign@gmail.com>
2020-12-24 11:34:40 -0800

    added CORS

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const config = require('config')
+const cors = require("cors")
+
+app.use(cors())
 
 const app = express();
 
